4bf1e33cd90e3dc295dbb913aa83a4ba485d97c2
tahsin hafsa <nthhafsa@gmail.com>
2024-02-19 14:40:37 +0600

    tenth commit

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,5 +1,8 @@
 
 // code for to go to bus seat and ticket section
+document.getElementById('seat-id').addEventListener('click',function(){
+    this.style.backgroundColor = "green";
+})
 
 document.getElementById('buy').addEventListener('click',function(){
     window.location.hash = '#bus';
@@ -16,6 +19,8 @@ function next(){
 }
 
 
+
+
 // calculation
 
 const alBtn = document.querySelectorAll('#seat-id');
@@ -25,7 +30,11 @@ let decrease =16;
 for(const btn of alBtn){
    btn.addEventListener('click',function(e){
     count = count + 1;
+    
+
     setInnerText('seat',count)
+
+    
    })
 }
 
@@ -40,6 +49,63 @@ for(const btn of alBtn){
         setInnerText('seat-count',decrease)
     })
 }
+
+function setInnerText(id,value){
+    document.getElementById(id).innerText = value;
+}
+
+
+// for price
+let price = 0
+
+
+
+for(const btn of alBtn){
+    btn.addEventListener('click',function(e){
+     price = price + 550;
+     
+ 
+     setInnerText('seat-price',price);
+ 
+     
+    })
+ }
+ 
+ function setInnerText(id,value){
+     document.getElementById(id).innerText = value;
+ }
+
+let className = '';
+ for(const btn of alBtn){
+    btn.addEventListener('click',function(e){
+     className = 'Economy';
+     
+ 
+     setInnerText('class-name',className);
+ 
+     
+    })
+ }
+ 
+
+
+// adding coupon
+document.getElementById('apply-code').addEventListener('click',function(){
+    let applyCode = document.getElementById('coupon').value;
+    let codeDiscount = 0;
+    if(applyCode === 'NEW15'){
+        codeDiscount = price * 0.15;
+    }
+    else if(applyCode === 'Couple20'){
+        codeDiscount = price * 0.2;
+    }
+
+    // price -= codeDiscount;
+    setInnerText('seat-price',price);
+
+    let grandTotal = price - codeDiscount;
+       setInnerText('grand',grandTotal);
+});
 
 function setInnerText(id,value){
     document.getElementById(id).innerText = value;
